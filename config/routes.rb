6ccca99c569866caddef44f797da32d33dafda93
rmeoni <rodrigo.me13@gmail.com>
2016-07-16 14:25:28 -0600

    Adding about us page and changing background attachment scroll for mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/' => 'pages#home', as: :root
+  get '/about' => 'pages#about'
   get "*missing" => redirect("/")
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
