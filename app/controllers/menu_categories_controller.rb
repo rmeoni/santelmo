078b69f5b66a8c72ffc7e214f7c30e3635916d79
rmeoni <rodrigo.me13@gmail.com>
2016-07-26 11:45:15 -0600

    Adding the menu

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -1,6 +1,6 @@
 class MenuCategoriesController < ApplicationController
 	def index
 		@menu_categories = Menu_category.all
-		@category_items = @menu_categories.category_items
+		@category_items = Category_item.all
 	end
 end
